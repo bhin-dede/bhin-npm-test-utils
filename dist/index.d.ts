752a09a46b2d827e0781468f90d50f5abafe672d
joo00206 <joo00206@gractor.com>
2024-10-08 16:48:26 +0900

    0.0.6 typescript적용

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+declare function add(a: number, b: number): number;
+declare function subtract(a: number, b: number): number;
+declare function multiply(a: number, b: number): number;
+declare function divide(a: number, b: number): number;
