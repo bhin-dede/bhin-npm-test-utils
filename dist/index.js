a0ff7e97d2b713f14dd429ae0a968c897ac87a46
joo00206 <joo00206@gractor.com>
2024-10-11 11:24:46 +0900

    0.0.7 > 파일에서 import error 0.0.8 > 터미널에서 TypeError

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = add;
+exports.subtract = subtract;
+exports.multiply = multiply;
+exports.divide = divide;
 function add(a, b) {
     return a + b;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-function add(a: number, b: number) {
+function add(a: number, b: number): number {
   return a + b;
 }
-function subtract(a: number, b: number) {
+
+function subtract(a: number, b: number): number {
   return a - b;
 }
-function multiply(a: number, b: number) {
+
+function multiply(a: number, b: number): number {
   return a * b;
 }
-function divide(a: number, b: number) {
+
+function divide(a: number, b: number): number {
   return a / b;
 }
+
 module.exports = {
   add,
   subtract,
   multiply,
   divide,
 };
+
+export { add, subtract, multiply, divide };
